8b31fe67c0735de19a65e6676250e57379c7c238
David Mood <davidmood@hotmail.com>
2021-09-25 21:28:37 +0330

    Adding the search cards component.

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { useRouter } from "next/dist/client/router"
 import { format } from "date-fns"
+import InfoCard from "../components/InfoCard";
 
 function search({searchResults}) {
     const router = useRouter();
@@ -49,6 +50,25 @@ function search({searchResults}) {
                         <div className="chip">Rooms and Beds</div>
                         <div className="chip">More Filters</div>
                     </div>
+                    <div className="flex flex-col">
+                        {searchResults.map(
+                            ({img, location, title, description, star, price, total}) => (
+                                <InfoCard 
+                                    key={img} 
+                                    img={img} 
+                                    location={location} 
+                                    title={title} 
+                                    description={description} 
+                                    star={star} 
+                                    price={price} 
+                                    total={total}
+                                />
+                            )
+                        )}
+                    </div>
+                </section>
+                <section >
+                    
                 </section>
             </main>
             <div className="bg-gray-100"><Footer /></div>
@@ -61,7 +81,7 @@ export default search;
 
 export async function getServerSideProps() {
     // const searchData = await fetch('url-to-fetch-api').then(res => res.json());
-    const sampleSearchData = await new Array([
+    const sampleSearchData = await new Array(
         {
             "img": "https://a0.muscache.com/im/pictures/e2e2bf3d-cb57-4ef0-9076-1571500da682.jpg?im_w=1440",
             "location": "Private room in center of London",
@@ -74,7 +94,7 @@ export async function getServerSideProps() {
             "lat": 51.5421655
         },
         {
-            "img": "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
+            "img": "https://images.pexels.com/photos/9582660/pexels-photo-9582660.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
             "location": "Private room in center of London",
             "title": "Independant luxury studio apartment",
             "description": "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
@@ -85,7 +105,7 @@ export async function getServerSideProps() {
             "lat": 51.48695
         },
         {
-            "img": "https://www.smartertravel.com/wp-content/uploads/2017/07/Untitled-design-8.jpg",
+            "img": "https://images.pexels.com/photos/2029722/pexels-photo-2029722.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
             "location": "Private room in center of London",
             "title": "London Studio Apartments",
             "description": "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
@@ -96,7 +116,7 @@ export async function getServerSideProps() {
             "lat": 51.521916
         },
         {
-            "img": "https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI",
+            "img": "https://images.pexels.com/photos/271643/pexels-photo-271643.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
             "location": "Private room in center of London",
             "title": "30 mins to Oxford Street, Excel London",
             "description": "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
@@ -107,7 +127,7 @@ export async function getServerSideProps() {
             "lat": 51.472618
         },
         {
-            "img": "https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg",
+            "img": "https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
             "location": "Private room in center of London",
             "title": "Spacious Peaceful Modern Bedroom",
             "description": "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning",
@@ -118,7 +138,7 @@ export async function getServerSideProps() {
             "lat": 51.510794
         },
         {
-            "img": "https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937",
+            "img": "https://images.pexels.com/photos/3201763/pexels-photo-3201763.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
             "location": "Private room in center of London",
             "title": "The Blue Room In London",
             "description": "2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine",
@@ -129,7 +149,7 @@ export async function getServerSideProps() {
             "lat": 51.51401
         },
         {
-            "img": "https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp",
+            "img": "https://images.pexels.com/photos/3769710/pexels-photo-3769710.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
             "location": "Private room in center of London",
             "title": "5 Star Luxury Apartment",
             "description": "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
@@ -138,7 +158,7 @@ export async function getServerSideProps() {
             "total": "£650 total",
             "long": -0.109889,
             "lat": 51.521245
-        }]
+        }
     );
     return {
         props:{
